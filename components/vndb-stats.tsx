51a0c8764e15898ad9d5be21b5c8e8145f8cdf77
sena <lingfeng-saya@yandex.com>
2023-10-09 12:08:19 +0800

    add zh-hans patch list

--- components/vndb-stats.tsx
+++ components/vndb-stats.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import style from "../styles/vndb-stats.module.css";
 import { Col, Row } from "react-bootstrap";
 import cn from "classnames";
+import path from "path";
 
 export default async function VndbStats({ vndbId }: { vndbId: string }) {
 
@@ -11,7 +12,7 @@ export default async function VndbStats({ vndbId }: { vndbId: string }) {
     let basicInfo = null;
     let releaseInfo = null;
     let basic_info = null;
-    let release_info = null;
+    let zh_has_patch = new Array();
 
     const vndbAPI = async (apiUrl: string, requestData: Record<string, any>) => {
         const headers: Record<string, string> = {
@@ -53,7 +54,7 @@ export default async function VndbStats({ vndbId }: { vndbId: string }) {
         return data.results[0];
     };
 
-    const getReleasecInfo = async (name: String) => {
+    const getReleasecInfo = async () => {
         const apiUrl = "https://api.vndb.org/kana/release";
         const requestData = {
             filters: ["and", ["patch", "=", 1], ["vn", "=", ["id", "=", vndbId]]],
@@ -78,27 +79,28 @@ export default async function VndbStats({ vndbId }: { vndbId: string }) {
 
     }
 
-    const length_time = function (min) {
+    const length_time = function (min : number ) {
         return ((min / 60) + "小时 " + (min % 60) + "min")
     }
 
-    const languages_flag = function (languages,language) {
+    const languages_flag = function (languages : any,  language : String) {
         let result = null;
         for (const element of languages) {
-          if (element === language) {
-            result = "有";
-            break;
-          }
+            if (element === language) {
+                result = "有";
+                break;
+            }
         }
-        
+
         return result
 
     }
-    const devstatu = function (stau) {
+    const devstatu = function (stau : number ) {
         switch (stau) {
             case 0: return "已发布";
             case 1: return "制作中";
             case 2: return "已跑路";
+            default : return "火星了";
         }
     }
 
@@ -108,7 +110,7 @@ export default async function VndbStats({ vndbId }: { vndbId: string }) {
     if (vndbId !== null && getBasicInfo(vndbId) != null) {
         basicInfo = await getBasicInfo(vndbId);
 
-        releaseInfo = await getReleasecInfo(basicInfo.title);
+        releaseInfo = await getReleasecInfo();
         console.log("===================basicinfo==================");
         console.log(basicInfo);
         console.log("===================basicinfo==================");
@@ -119,16 +121,32 @@ export default async function VndbStats({ vndbId }: { vndbId: string }) {
         basic_info = {
             "游戏名": basicInfo.title,
             "原语言": basicInfo.olang,
-            "游戏状态":devstatu(basicInfo.devstatus),
+            "游戏状态": devstatu(basicInfo.devstatus),
             "发售日期": basicInfo.released,
 
-            "游戏长度":length_calculate(basicInfo.length) + "( " + length_time(basicInfo.length_minutes) + ") ",
-            "汉化版": languages_flag(basicInfo.languages,"zh-Hans"),
-            "英文版": languages_flag(basicInfo.languages,"en"),
+            "游戏长度": length_calculate(basicInfo.length) + "( " + length_time(basicInfo.length_minutes) + ") ",
+            "汉化版": languages_flag(basicInfo.languages, "zh-Hans"),
+            "英文版": languages_flag(basicInfo.languages, "en"),
 
             "平均分数": basicInfo.rating,
             "投票人数": basicInfo.votecount,
         }
+
+
+
+        for (const release of releaseInfo) {
+            if (release.languages[0].lang === "zh-Hans") {
+                release.lanuage = "zh-Hans";
+                delete release.languages;
+                zh_has_patch.push(release);
+                release.extlink = release.extlinks[0]?.url
+                delete release.extlinks
+            };
+
+        }
+
+        console.log(zh_has_patch);
+
 
 
 
@@ -157,7 +175,7 @@ export default async function VndbStats({ vndbId }: { vndbId: string }) {
                                 <tr key={key}>
                                     <td>{key}</td>
                                     {/* <td>{Array.isArray(value) ? value.join(" ") : value}</td> */}
-                                    <td>{value === null ? "未知" : value }</td>
+                                    <td>{value === null ? "未知" : value}</td>
                                 </tr>
                             ))}
                         </tbody>
@@ -165,11 +183,23 @@ export default async function VndbStats({ vndbId }: { vndbId: string }) {
                 </div>
             </Row>
             <Row className={style.dataContainer}>
-                <Col className={cn(style.rating, "col-12 col-md-6")}>評分和投票</Col>
-                <Col className={cn(style.details, "col-12 col-md-6")}>
+                <Col className={cn(style.rating, "col-12 col-md-6")}>汉化补丁</Col>
+                {zh_has_patch.map(patch => (
+                    <div>
+                          {Object.entries(patch).map(([key,value]) => (
+                                <tr key={key}>
+                                    <td>{key}</td>
+                                    {/* <td>{Array.isArray(value) ? value.join(" ") : value}</td> */}
+                                    <td>{value === null ? "未知" : value}</td>
+                                </tr>
+                            ))}
+                    </div>
+                ))}
+
+                {/* <Col className={cn(style.details, "col-12 col-md-6")}>
                     <Link href={`/vndb/${vndbId}/release`}></Link>
                     <div className={style.releases}>Releases</div>
-                </Col>
+                </Col> */}
             </Row>
         </div>
     );
