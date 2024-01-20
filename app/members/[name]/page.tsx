1f441a76b8e001c35786b3709e7ac099cfc91e1e
Kiriha <kiriha@yamabuki.edu>
2024-01-21 01:10:04 +0900

    update member profile page

--- app/members/[name]/page.tsx
+++ app/members/[name]/page.tsx
@@ -7,58 +7,70 @@ import BookmarkContainer from "@/components/bookmark-container";
 import cn from "classnames";
 
 
-export async function generateStaticParams() {
-    const params = Object.keys({ ...staffs, ...members }).map((name) => ({
-        name: encodeURIComponent(name),
-    }));
+export function generateStaticParams() {
+    const params = Object.keys({ ...staffs, ...members }).map((member) => {
+        return { name: member, };
+    });
+
+    const temp = [];
+    params.forEach((param) => {
+        const encoded = encodeURIComponent(param.name);
+        if (encoded !== param.name) {
+            temp.push({ name: encoded });
+        }
+    });
+    params.push(...temp);
 
     return params;
 }
 
 
 
-export default function Member({ params }) {
+export default function MemberProfile({ params } : { params: { name: string } }) {
     const name = decodeURIComponent(params.name);
+    // console.log(name);
 
     const member_info = getMember(name);
 
     if (member_info === undefined || !member_info.personalPage) {
-        return <DefaultCharacterPages members={member_info}  />;
+        return <DefaultCharacterPages name={name} member={member_info} />;
     }
 
-    <CharacterPages members={member_info} />
+    <CharacterPages name={name} member={member_info} />
 
 }
-function CharacterPages({ members }: { members: member }) {
+
+
+function CharacterPages({ name, member }: { name: string, member: member }) {
     return (
         <Container className="px-2">
             <BookmarkContainer title={
-                <>{members.name}</>
+                <>{name}</>
             }>
             </BookmarkContainer>
 
         </Container>
 
     );
-
 }
-function DefaultCharacterPages({ members }: { members: member }) {
+
+function DefaultCharacterPages({ name, member }: { name: string, member: member }) {
     return (
         <Container className="px-2">
             <BookmarkContainer title={
-                <>{members.name}</>
+                <><i className="bi bi-person-heart me-1" />{name}</>
             }>
                 <div className="container myfont">
                     <p className={style.defaultText}>
-                        因为 <b>{members.name}</b> 没有提供个人主页内容，所以该页面是简陋的默认页面。
+                        因为 <b>{name}</b> 没有提供个人主页内容，所以该页面是简陋的默认页面。
                     </p>
                     <div className={cn(`${style.InfoPhoto} d-flex flex-column flex-sm-row`)}>
                         <div className={cn(`${style.linkAvatar} col-lg-3 col-xl-2`)}>
-                            <img src={members.photo} alt={members.name} />
+                            <img src={member.photo} alt={name} />
                         </div>
                         <div className={cn(`${style.InfoText} col-lg-9 col-xl-10`)}>
                             <div className={cn(`${style.InfoBio} font-serif`)}>
-                                {members.bio !== "" ? `"${members.bio}"` : "这是一个默认Bio(这个家伙懒爆了，Bio都不提供)"}
+                                {member.bio !== "" ? `"${member.bio}"` : "这是一个默认 Bio (这个家伙懒爆了，Bio 都不提供)"}
                             </div>
                         </div>
                     </div>
--- components/bookmark-container.tsx
+++ components/bookmark-container.tsx
@@ -6,8 +6,8 @@ import style from '@/styles/bookmark-container.module.css';
 export default function BookmarkContainer({ title, children }: { title: ReactNode, children?: ReactNode }) {
     return (
       <section className={style.bookmarkContainer}>
-        <div className={cn(style.bookmark, 'box-shadow p-2 ms-4 ms-md-5')}>
-          <div className={cn('d-flex o-hidden align-items-center h-100', style.sectionTitle)}>
+        <div className={cn(style.bookmark, 'box-shadow p-2 ms-4 ms-md-5 pe-5')}>
+          <div className={cn('d-flex align-items-center h-100', style.sectionTitle)}>
             <div className={cn('ms-2')}>
               <h2 className={cn('text-decoration-none fw-bold fst-italic h3')}>
                 {title}
