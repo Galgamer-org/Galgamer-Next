0eebf6865499c5d424e817d338ffe7844117d96a
Ki mi ka <66236255+Yamabuki-bakery@users.noreply.github.com>
2024-01-21 19:24:10 +0900

    avatar clickable

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,10 +1,8 @@
-import Member from "interfaces/member"
-import Image from "next/image"
 import style from "styles/avatar.module.css"
 import cn from 'classnames'
-import { Col } from "react-bootstrap"
 import SmartImage from "./smart-image"
 import { getMember } from "_feed/members";
+import Link from "next/link";
 
 
 type Props = {
@@ -15,14 +13,16 @@ export default function Avatar ({ name }: Props) {
   const member = getMember(name);
   const { photo, bio, } = member;
   return (
-    <div className={cn(style.container ,"d-flex align-items-center")}>
-      <div className={cn(style.photoContainer ,"mr-2")}>
-        <SmartImage src={photo} alt={`${name}'s profile photo`} className={cn(style.avatar, "rounded-full")} />
+    <Link href={`/members/${name}`} className={cn('d-block', style.link)}>
+      <div className={cn(style.container ,"d-flex align-items-center")}>
+          <div className={cn(style.photoContainer ,"mr-2")}>
+            <SmartImage src={photo} alt={`${name}'s profile photo`} className={cn(style.avatar, "rounded-full")} />
+          </div>
+          
+          <div className={cn(style.name ,"font-weight-bold mx-2 myfont")}>
+            {name}
+          </div>
       </div>
-      
-      <div className={cn(style.name ,"font-weight-bold mx-2")}>
-        {name}
-      </div>
-    </div>
+    </Link>
   )
 }
