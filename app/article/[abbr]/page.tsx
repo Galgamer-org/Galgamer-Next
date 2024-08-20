7449ece613303eba74bebefbd25beca023525f7a
Misaki <misaki@yamabuki.ac.jp>
2024-08-20 18:53:49 +0900

    article layout

--- app/article/[abbr]/page.tsx
+++ app/article/[abbr]/page.tsx
@@ -1,12 +1,13 @@
-import { getAllPosts, getPostByAbbrlink } from '@/lib/api'
-import markdownToHtml from '@/lib/markdownToHtml'
-import Container from '@/components-layout/container'
-import PostHeader from '@/components/post-header'
-import { ReactNode } from 'react'
-import type { Metadata, ResolvingMetadata } from 'next'
-import cn from 'classnames'
-import '@/styles/github-markdown.css'
-import style from '@/styles/article.module.css'
+import { getAllPosts, getPostByAbbrlink } from "@/lib/api";
+import markdownToHtml from "@/lib/markdownToHtml";
+import Container from "@/components-layout/container";
+import PostHeader from "@/components/post-header";
+import { ReactNode } from "react";
+import type { Metadata, ResolvingMetadata } from "next";
+import cn from "classnames";
+import "@/styles/github-markdown.css";
+import style from "@/styles/article.module.css";
+import SmartImage from "@/components/smart-image";
 
 //export const dynamicParams = false;
 export async function generateStaticParams() {
@@ -14,8 +15,8 @@ export async function generateStaticParams() {
   let result = posts.map((post) => {
     return {
       abbr: post.abbrlink.toString(),
-    }
-  })
+    };
+  });
   //console.log(result)
   return result.slice(0, 10);
 }
@@ -33,7 +34,7 @@ export async function generateMetadata(
   const post = getPostByAbbrlink(parseInt(abbr));
 
   return {
-    metadataBase: new URL('https://galgamer.moe'),
+    metadataBase: new URL("https://galgamer.moe"),
     title: post.title,
     description: post.excerpt,
     keywords: post.keywords,
@@ -41,24 +42,24 @@ export async function generateMetadata(
     openGraph: {
       title: post.title,
       description: post.excerpt,
-      url: 'https://nextjs.org',
-      siteName: 'Galgamer',
+      url: "https://nextjs.org",
+      siteName: "Galgamer",
       images: [
         {
-          url: post.index_img,  // need fix
-        }
+          url: post.index_img, // need fix
+        },
       ],
-      locale: 'zh_HK',
-      type: 'website',
+      locale: "zh_HK",
+      type: "website",
     },
 
     twitter: {
-      card: 'summary_large_image',
+      card: "summary_large_image",
       title: post.title,
       description: post.excerpt,
-      images: [post.index_img],  // need fix
+      images: [post.index_img], // need fix
     },
-  }
+  };
 }
 
 
@@ -71,22 +72,41 @@ export default async function Article(
   const content = markdownToHtml(post.content)
 
   return (
-    <Container className='px-2'>
+    <>
       <CustomCss />
-      <PostHeader
-        title={post.title}
-        index_img={post.index_img}
-        date={post.date}
-        author={post.author}
-        excerpt={post.excerpt}
-      />
+      <Container className="px-2">
+        <PostHeader
+          title={post.title}
+          index_img={post.index_img}
+          date={post.date}
+          author={post.author}
+          excerpt={post.excerpt}
+        />
+      </Container>
 
-      <section className="d-flex">
-        <PostBody content={content} />
-        <Aside />
-      </section>
+      <HeroImage src={post.index_img} />
+      <Container className="px-2 mt-4">
+        <section className="d-flex">
+          <PostBody content={content} />
+          <Aside />
+        </section>
+      </Container>
+    </>
+  );
+}
 
-    </Container>
+function HeroImage({ src }: { src: string }) {
+  return (
+    <section className={cn(style.heroContainer, "mx-auto mt-3 px-2")}>
+      <div className={cn(style.cardContainer, "box-shadow")}>
+          <SmartImage
+            src={src}
+            alt={`Cover Image`}
+            className={cn("w-100", style.coverImage)}
+            loading="eager"
+          />
+      </div>
+    </section>
   );
 }
 
@@ -97,43 +117,36 @@ function CustomCss() {
         background: var(--body-bg-color);
       }
     `}</style>
-  )
+  );
 }
 
 function Aside() {
   return (
-    <div className={cn('d-lg-block d-none col-3')}>
-      <aside className={cn('ps-3', style.asideContent)}>
-        <div className={cn('container-board p-3 mb-3 box-shadow o-hidden')}>
-          <div className='fw-bolder'>目錄</div>
+    <div className={cn("d-lg-block d-none col-3")}>
+      <aside className={cn("ps-3", style.asideContent)}>
+        <div className={cn("container-board p-3 mb-3 box-shadow o-hidden")}>
+          <div className="fw-bolder">目錄</div>
           <div className="">
             <img src="https://via.placeholder.com/200x200" alt="廣告" />
           </div>
         </div>
-        <div className={cn('container-board p-3 box-shadow o-hidden')}>
-          <div className='fw-bolder'>垃圾</div>
+        <div className={cn("container-board p-3 box-shadow o-hidden")}>
+          <div className="fw-bolder">垃圾</div>
           <div className="">
             <img src="https://via.placeholder.com/200x200" alt="廣告" />
           </div>
         </div>
       </aside>
     </div>
-
   );
 }
 
-function PostBody({ content }: {
-  content: ReactNode
-}) {
+function PostBody({ content }: { content: ReactNode }) {
   return (
-    <article className='col-12 col-lg-9'>
-      <div className={cn('container-board', 'mx-auto p-3 box-shadow')}>
-        <div
-          className={cn('markdown-body', 'p-3')}
-          children={content}
-        />
+    <article className="col-12 col-lg-9">
+      <div className={cn("container-board", "mx-auto p-3 box-shadow")}>
+        <div className={cn("markdown-body", "p-3")} children={content} />
       </div>
     </article>
-  )
+  );
 }
-
